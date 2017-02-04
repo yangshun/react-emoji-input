7ea1d09e0d3b3693bf29aa175a16cff7a9cd0902
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-02-04 23:04:59 +0800

    Add a simple prop to FooBar

--- __tests__/FooBar.test.js
+++ __tests__/FooBar.test.js
@@ -6,6 +6,12 @@ import FooBar from '../src/FooBar';
 describe('<FooBar/>', () => {
   it('should render Foo Bar', () => {
     const wrapper = shallow(<FooBar />);
-    expect(wrapper.text()).toBe('Foo Bar');
+    expect(wrapper.find('h3').text()).toBe('Foo Bar');
+  });
+
+  it('should render Foo Bar with contents', () => {
+    const wrapper = shallow(<FooBar contents="Some contents." />);
+    expect(wrapper.find('h3').text()).toBe('Foo Bar');
+    expect(wrapper.find('p').text()).toBe('Some contents.');
   });
 });
--- src/FooBar.jsx
+++ src/FooBar.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+
+const styles = {
+  border: '1px solid #ccc',
+  borderRadius: 10,
+  fontFamily: '"Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif',
+  maxWidth: 400,
+  padding: 10,
+};
 
 class FooBar extends Component {
   render() {
     return (
-      <div>Foo Bar</div>
+      <div style={styles}>
+        <h3>Foo Bar</h3>
+        {this.props.contents && <p>{this.props.contents}</p>}
+      </div>
     );
   }
 }
+
+FooBar.propTypes = {
+  contents: PropTypes.string,
+};
 
 export default FooBar;
